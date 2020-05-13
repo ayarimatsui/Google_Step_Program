f56ef2794dd470329b6c3726329a69b7bdfe78af
松井彩里 <matsuiayari@matsuiayasatonoMacBook-Pro.local>
2020-05-13 16:40:56 +0900

    added quiz/Boyer-Moore Voting Algorithm

---
+++ quiz/Boyer_Moore_Voting_Algorithm.py
@@ -0,0 +1,38 @@
+# Boyer-Moore Voting Algorithm
+# 過半数を占めるものがある時、O(1)のメモリを使ってその値を当てる方法
+
+import sys
+def judge_majority():
+    count = 0
+    cand = None
+    while True:
+        n = sys.stdin.readline()[:-1] # 改行コードを取り除いて入力を取得
+        if str.isdecimal(n):  # 入力が数字に変換可能かどうかを確かめる
+            num = int(n)
+            if count == 0:
+                cand = num
+            if num == cand:
+                count += 1
+            else:
+                count -= 1
+        else:  # 入力が数字に変換できない時、つまり数字が流れてくるのが終わった時、ループから抜ける
+            break
+    return cand
+ans = judge_majority()
+print(ans)
+
+'''
+(入力例)
+1
+4
+5
+5
+5
+10
+5
+10
+2
+5
+5
+5
+'''
