3c40ef12739f836ac753836a016611abaa0b3267
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-05-22 23:57:46 +0900

    added draw_graph.py

---
+++ week_2/homework1/draw_graph.py
@@ -0,0 +1,29 @@
+# matrix_python.csv, matrix_numpy.csv, matrix_c.csvに保存したデータを読み込み、グラフを描く
+# 行列の積を求めるプログラムの実行時間を、Python, Numpy, Cで比較する
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+python_file = open('matrix_python.csv', 'r')
+reader1 = csv.reader(python_file)
+python_time = [float(row[1]) for row in reader1]
+
+numpy_file = open('matrix_numpy.csv', 'r')
+reader2 = csv.reader(numpy_file)
+numpy_time = [float(row[1]) for row in reader2]
+
+c_file = open('matrix_c.csv', 'r')
+reader3 = csv.reader(c_file)
+c_time = [float(row[1]) for row in reader3]
+
+x = list(range(2, 100))
+
+# グラフの描画
+plt.plot(x, python_time, label="Python")
+plt.plot(x, numpy_time, label="Numpy")
+plt.plot(x, c_time, label="C")
+plt.title("Execution Time")
+plt.xlabel("size of N")
+plt.ylabel("time (s)")
+plt.legend(loc="best")
+plt.show()
