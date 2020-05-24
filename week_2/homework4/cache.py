4f18d8bd252aa99b989cd8a014255549bad4bb00
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-05-24 13:27:23 +0900

    modified cache.py

--- week_2/homework4/cache.py
+++ week_2/homework4/cache.py
@@ -12,7 +12,7 @@ class Cache:
   def __init__(self, n):
     self.size = n
     self.hashmap = HashTable(self.size)
-    self.queue = deque([None] * self.size)　　# 最新のn個のurlを入れておくためのキュー
+    self.queue = deque([None] * self.size)   # 最新のn個のurlを入れておくためのキュー
     pass
 
 
@@ -48,7 +48,7 @@ class Cache:
   def get_pages(self):
     pages_list = []
     hashtable = self.hashmap.hashmap
-    for url in reverse(self.queue):
+    for url in reversed(self.queue):
         if url is None:
             continue
         if self.hashmap.contains(url):  # キューの中のurlがハッシュテーブルにあることを確認
@@ -62,66 +62,54 @@ def cache_test():
   # Set the size of the cache to 4.
   cache = Cache(4)
   # Initially, no page is cached.
-  print(cache.get_pages())
-  #equal(cache.get_pages(), [])
+  equal(cache.get_pages(), [])
   # Access "a.com".
   cache.access_page("a.com", "AAA")
   # "a.com" is cached.
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["a.com"])
+  equal(cache.get_pages(), ["a.com"])
   # Access "b.com".
   cache.access_page("b.com", "BBB")
   # The cache is updated to:
   #   (most recently accessed)<-- "b.com", "a.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["b.com", "a.com"])
+  equal(cache.get_pages(), ["b.com", "a.com"])
   # Access "c.com".
   cache.access_page("c.com", "CCC")
   # The cache is updated to:
   #   (most recently accessed)<-- "c.com", "b.com", "a.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["c.com", "b.com", "a.com"])
+  equal(cache.get_pages(), ["c.com", "b.com", "a.com"])
   # Access "d.com".
   cache.access_page("d.com", "DDD")
   # The cache is updated to:
   #   (most recently accessed)<-- "d.com", "c.com", "b.com", "a.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["d.com", "c.com", "b.com", "a.com"])
+  equal(cache.get_pages(), ["d.com", "c.com", "b.com", "a.com"])
   # Access "d.com" again.
   cache.access_page("d.com", "DDD")
   # The cache is updated to:
   #   (most recently accessed)<-- "d.com", "c.com", "b.com", "a.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["d.com", "c.com", "b.com", "a.com"])
+  equal(cache.get_pages(), ["d.com", "c.com", "b.com", "a.com"])
   # Access "a.com" again.
   cache.access_page("a.com", "AAA")
   # The cache is updated to:
   #   (most recently accessed)<-- "a.com", "d.com", "c.com", "b.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["a.com", "d.com", "c.com", "b.com"])
+  equal(cache.get_pages(), ["a.com", "d.com", "c.com", "b.com"])
   cache.access_page("c.com", "CCC")
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["c.com", "a.com", "d.com", "b.com"])
+  equal(cache.get_pages(), ["c.com", "a.com", "d.com", "b.com"])
   cache.access_page("a.com", "AAA")
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["a.com", "c.com", "d.com", "b.com"])
+  equal(cache.get_pages(), ["a.com", "c.com", "d.com", "b.com"])
   cache.access_page("a.com", "AAA")
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["a.com", "c.com", "d.com", "b.com"])
+  equal(cache.get_pages(), ["a.com", "c.com", "d.com", "b.com"])
   # Access "e.com".
   cache.access_page("e.com", "EEE")
   # The cache is full, so we need to remove the least recently accessed page "b.com".
   # The cache is updated to:
   #   (most recently accessed)<-- "e.com", "a.com", "c.com", "d.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["e.com", "a.com", "c.com", "d.com"])
+  equal(cache.get_pages(), ["e.com", "a.com", "c.com", "d.com"])
   # Access "f.com".
   cache.access_page("f.com", "FFF")
   # The cache is full, so we need to remove the least recently accessed page "c.com".
   # The cache is updated to:
   #   (most recently accessed)<-- "f.com", "e.com", "a.com", "c.com" -->(least recently accessed)
-  print(cache.get_pages())
-  #equal(cache.get_pages(), ["f.com", "e.com", "a.com", "c.com"])
+  equal(cache.get_pages(), ["f.com", "e.com", "a.com", "c.com"])
   print("OK!")
 
 # A helper function to check if the contents of the two lists is the same.
