35af4e473e639897f86617d748b4beca3d5a5f10
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-06-07 02:34:16 +0900

    updated homework1.py

--- week_4/homework1/homework1.py
+++ week_4/homework1/homework1.py
@@ -23,6 +23,7 @@ class Node:
 
 
 # breadth first search
+# search the path and distance from start to goal
 def bfs(node_list, start, goal):
     N = len(node_list)   # total number of nodes
     # create queue and append root node
@@ -40,7 +41,7 @@ def bfs(node_list, start, goal):
         distance = node_list[node_id].get_dist()
 
         if node_id == goal:
-            print('reached {} from {}!!'.format(node_list[start].nickname, node_list[goal].nickname))
+            print('reached {} from {}!!'.format(node_list[goal].nickname, node_list[start].nickname))
             return distance
 
         for user_id in node_list[node_id].follows:
@@ -50,6 +51,46 @@ def bfs(node_list, start, goal):
                 node_list[user_id].input_dist(distance + 1)
 
     return None
+
+
+# find the farthest node from the root node
+# find by bfs
+# also find whether there are nodes that cannot be reached from root node
+def find_farthest(node_list, root_node):
+    N = len(node_list)   # total number of nodes
+    # create queue and append root node
+    q = deque()
+    q.append(root_node)
+    node_list[root_node].input_dist(0)  # set the distance of the root node
+    # create a list to check whether nodes are visited or not
+    check_list = [0] * N  # 1 if already checked
+    check_list[root_node] = 1
+
+    # repeat while the queue is not empty
+    while len(q) > 0:
+        # take out the first node in the queue
+        node_id = q.popleft()
+        distance = node_list[node_id].get_dist()
+
+        for user_id in node_list[node_id].follows:
+            if check_list[user_id] == 0:  # if the node is unchecked
+                check_list[user_id] = 1
+                q.append(user_id)
+                node_list[user_id].input_dist(distance + 1)
+
+        # if the node is the last one
+        if len(q) == 0:
+            print('the farthest node from {} is {}'.format(node_list[root_node].nickname, node_list[node_id].nickname))
+            print('the distance is {}'.format(distance))
+
+    # return unvisited nodes (the nodes cannot be reached from root node)
+    unvisited = []
+    for id, checked in enumerate(check_list):
+        if checked == 0:
+            unvisited.append(node_list[id].nickname)
+
+    return unvisited
+
 
 
 if __name__ == '__main__':
@@ -76,9 +117,17 @@ if __name__ == '__main__':
             node_list[ID].add_follow(follow)
             node_list[follow].add_follower(ID)
 
+    # check whether my account(jamie) can be reached from target(adrian)
     distance_from_target = bfs(node_list, target_id, my_id)
-
     if distance_from_target is None:
         print('There is no path from adrian to me')
     else:
-        print('the distance from target : ' + str(distance_from_target))
+        print('the distance from {} to {} : {}'.format(node_list[target_id].nickname, node_list[my_id].nickname, distance_from_target))
+
+    # check the farthest node from my account
+    # also find whether there are nodes that cannot be reached from my account
+    unreachable = find_farthest(node_list, my_id)
+    if len(unreachable) > 0:
+        print('unreachable accounts from {} : {}'.format(node_list[my_id].nickname, ' '.join(unreachable)))
+    else:
+        print('every account is reachable from {}'.format(node_list[my_id].nickname))
