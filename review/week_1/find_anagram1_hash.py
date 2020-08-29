64825a576c83b6d3c1ec086af55a8b2170aed283
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-08-29 11:07:50 +0900

    added find_anagram program using hashmap

--- review/week_1/find_anagram1_hash.py
+++ review/week_1/find_anagram1_hash.py
@@ -1,3 +1,47 @@
 # Google STEP 2020  Week1 Homework1  Review
 # 普通のアナグラムを辞書から探して返すプログラム
 # ハッシュテーブルを利用
+
+from hash_table import HashTable
+import time
+
+
+# 辞書のデータファイルを読み込む関数
+def read_dic(file):
+    # 辞書データのファイルを読み込み、全て小文字に変換してリストdictionaryに格納
+    with open(file) as f:
+        dictionary = [s.strip().lower() for s in f]
+    return dictionary
+
+
+# ハッシュテーブルを作る関数
+def make_hashmap(dictionary):
+    hashmap = HashTable()
+    for word in dictionary:
+        hashmap.put(word)
+    return hashmap
+
+
+# アナグラムを見つける
+def find_anagram_with_hashmap(word, hashmap):
+    anagram_list = hashmap.get(word)
+    return anagram_list
+
+
+if __name__ == '__main__':
+    dic_file = 'dictionary_words.txt'
+    dictionary = read_dic(dic_file)
+    hashmap = make_hashmap(dictionary)
+    
+    try:
+        while True:
+            word = input('input a word : ')
+            ans_list = find_anagram_with_hashmap(word, hashmap)
+            if len(ans_list) == 0:
+                print('There is no anagram.')
+            else:
+                ans = ', '.join(ans_list)
+                print(ans)
+            time.sleep(0.3)
+    except KeyboardInterrupt:
+        print('!!FINISH!!')
--- review/week_1/hash_table.py
+++ review/week_1/hash_table.py
@@ -4,68 +4,41 @@ class HashTable:
 
     def __init__(self):
         self.size = 0
-        self.hashmap = [None] * 100000
+        self.hashmap = [None] * 1000000
 
     # ハッシュ関数
-    def hash(self, key):
+    def hash(self, word):
         p_dic = {'a': 2, 'b': 3, 'c': 5, 'd': 7, 'e': 11, 'f': 13, 'g': 17, 'h': 19, 'i': 23, 'j': 29, 'k': 31, 'l': 37, 'm': 41, 'n': 43, 'o': 47, 'p': 53, 'q': 59, 'r': 61, 's': 67, 't': 71, 'u': 73, 'v': 79, 'w': 83, 'x': 89, 'y': 97, 'z': 101}
-        idx = 0
-        for k in key:
-            idx *= (p_dic[k] % 100000)
-            idx %= 100000
+        idx = 1
+        for s in word:
+            idx *= p_dic[s]
+            idx %= 1000000
         return idx
 
-    # ハッシュテーブルに(key, value)を入れる
-    def put(self, key, value):
-        idx = self.hash(key)
+    # ハッシュテーブルにwordを入れる
+    def put(self, word):
+        idx = self.hash(word)
         if self.hashmap[idx] is None:
-            self.hashmap[idx] = [(key, value)]
+            self.hashmap[idx] = [word]
         else:
-            arr = self.hashmap[idx]
-            for i in range(len(arr)):
-                pair = arr[i]
-                if pair[0] == key:
-                    del arr[i]
-                    break
-            arr.append((key, value))
+            lst = self.hashmap[idx]
+            lst.append(word)
+            self.hashmap[idx] = lst
         self.size += 1
 
-    # keyを元にハッシュテーブルに保持されているvalueの値を返す (ない時は-1を返す)
-    def get(self, key):
+    # idxを元にハッシュテーブルに保持されている単語のリストを返す (ない時は空リストを返す)
+    def get(self, word):
         count = 1
-        idx = self.hash(key)
+        idx = self.hash(word)
         if self.hashmap[idx] is None:
-            return None
+            return []
         else:
-            arr = self.hashmap[idx]
-            for i in range(len(arr)):
-                pair = arr[i]
-                if pair[0] == key:
-                    return pair[1]
-            return None
+            lst = self.hashmap[idx]
+            anagram_lst = []
+            for elem in lst:
+                if elem != word:
+                    anagram_lst.append(elem)
+            return anagram_lst
 
-    # keyがハッシュテーブルの中にあるかどうかを調べる
-    def contains(self, key):
-        idx = self.hash(key)
-        if self.hashmap[idx] is None:
-            return False
-        else:
-            arr = self.hashmap[idx]
-            for i in range(len(arr)):
-                pair = arr[i]
-                if pair[0] == key:
-                    return True
-            return False
-
-    # keyの要素をハッシュテーブルから削除
-    def remove(self, key):
-        idx = self.hash(key)
-        arr = self.hashmap[idx]
-        for i in range(len(arr)):
-            pair = arr[i]
-            if pair[0] == key:
-                del arr[i]
-                self.size -= 1
-                break
 
 
